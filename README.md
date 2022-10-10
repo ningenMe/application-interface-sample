a75f241e171a08d089ec036d26671192f538d7a3
ningenMe <amohkuru@gmail.com>
2022-10-10 16:50:02 +0900

    [fix] dockerfile

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
 # application-interface-sample
 
-```shell
-GO_OUT_PATH="./server-grpc-go"
-protoc \
---go_out="${GO_OUT_PATH}" --go_opt=paths=source_relative \
---go-grpc_out="${GO_OUT_PATH}" --go-grpc_opt=paths=source_relative \
-./interface-grpc/*.proto
-```
-
+### generate
 ```shell
 docker-compose -f docker-compose-generate-interface.yml build --no-cache &&  docker-compose -f docker-compose-generate-interface.yml up 
 ```
+
--- generate-interface/generate.sh
+++ generate-interface/generate.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 protoc --version
-touch hoge.txt
+
+GO_OUT_PATH="./server-grpc-go"
+protoc \
+--go_out="${GO_OUT_PATH}" --go_opt=paths=source_relative \
+--go-grpc_out="${GO_OUT_PATH}" --go-grpc_opt=paths=source_relative \
+./interface-grpc/*.proto
+
+TS_OUT_PATH="./client-react"
+protoc \
+--js_out="import_style=commonjs,binary:${TS_OUT_PATH}" \
+--grpc-web_out="import_style=typescript,mode=grpcwebtext:${TS_OUT_PATH}" \
+./interface-grpc/*.proto
