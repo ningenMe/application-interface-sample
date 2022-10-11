6518a598d01c2a91b5516e518ae478dd69f1a279
ningenMe <amohkuru@gmail.com>
2022-10-12 07:07:45 +0900

    [fix] css

--- client-react/pages/index.tsx
+++ client-react/pages/index.tsx
@@ -1,57 +1,58 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Index.module.css'
+import commonStyles from '../styles/Common.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>client-react</title>
         <meta name="description" content="client-react" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={commonStyles.container}>
 
-      <main className={styles.main}>
-        <h2>
+        <main className={styles.main}>
+          <h2>
             Application Interface Sample
-
-        </h2>
-        <h1 className={styles.title}>
+          </h2>
+          <h1 className={styles.title}>
             client-react
-        </h1>
+          </h1>
 
-        <p className={styles.description}>
+          <p className={styles.description}>
             implemented by {' '}
-          <code className={styles.code}>next</code>
+            <code className={styles.code}>next</code>
             {' '} and {' '}
-          <code className={styles.code}>typescript</code>
-        </p>
+            <code className={styles.code}>typescript</code>
+          </p>
 
-        <div className={styles.grid}>
-          <a href="/server-grpc-go" className={styles.card}>
-            <h2>Interface: Grpc</h2>
-            <h2>Language: Go &rarr;</h2>
-          </a>
+          <div className={styles.grid}>
+            <a href="/server-grpc-go" className={styles.card}>
+              <h2>Interface: Grpc</h2>
+              <h2>Language: Go &rarr;</h2>
+            </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Interface: Websocket</h2>
-            <h2>Language: Kotlin &rarr;</h2>
-          </a>
+            <a href="https://nextjs.org/learn" className={styles.card}>
+              <h2>Interface: Websocket</h2>
+              <h2>Language: Kotlin &rarr;</h2>
+            </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Interface: Openapi</h2>
-            <h2>Language: Rust &rarr;</h2>
-          </a>
+            <a href="https://nextjs.org/learn" className={styles.card}>
+              <h2>Interface: Openapi</h2>
+              <h2>Language: Rust &rarr;</h2>
+            </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Interface: Graphql</h2>
-            <h2>Language: Kotlin &rarr;</h2>
-          </a>
+            <a href="https://nextjs.org/learn" className={styles.card}>
+              <h2>Interface: Graphql</h2>
+              <h2>Language: Kotlin &rarr;</h2>
+            </a>
 
-        </div>
-      </main>
-
-    </div>
+          </div>
+        </main>
+      </div>
+    </>
   )
 }
 
