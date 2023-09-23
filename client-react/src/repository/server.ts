ae1cf7e4ecb0b11a79c8122eac41bf64773ab891
ningenMe <amohkuru@gmail.com>
2023-09-24 04:01:47 +0900

    [fix]

--- client-react/src/repository/server.ts
+++ client-react/src/repository/server.ts
@@ -4,4 +4,4 @@ const getHostName = (port: string) => {
   return "http://localhost:" + port;
 }
 
-export const serverGrpcGoClient = new ChatServiceClient(getHostName(process.env.NEXT_PUBLIC_AIS_SERVER_GRPC_ENVOY_GO_PORT!!))
+export const serverGrpcGoClient = new ChatServiceClient(getHostName(process.env.NEXT_PUBLIC_AIS_SERVER_GO_GRPC_ENVOY_PORT!!))
